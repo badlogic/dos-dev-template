a253a3ed8188e180eb450f30f1cb8e6ad01a3802
Mario Zechner <badlogicgames@gmail.com>
2022-12-21 06:17:26 +0100

    Launcher config + tasks.

--- src/main.c
+++ src/main.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include "gdbstub.h"
 
+typedef struct point {
+    float x, y;
+} point;
+
 int main(void) {
     gdb_start();
 
-    printf("Hello DOS\n");
+    point p = { 1, 2 };
+    p.x = 34;
+    p.y = p.x + 2;
+
+    printf("Hello DOS, %f, %f\n", p.x, p.y);
     return 0;
 }
